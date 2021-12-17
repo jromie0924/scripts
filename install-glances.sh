a783ec20f295c617b16f143202f3fcda7863ab2a
Jackson Romie <jromie0924@gmail.com>
2021-12-16 19:20:08 -0700

    add gpustat to sensor software script

--- install-glances.sh
+++
@@ -1,1 +0,0 @@
-pip install -U glances py3nvml
---
+++ install-sensor-software.sh
@@ -0,0 +1,4 @@
+pip install -U glances py3nvml
+pip install -U gpustat
+
+print('\n\nRUN GLANCES WITH THIS COMMAND: \nglances\n\nRUN GPUSTAT WITH THIS COMMAND:\ngpustat -i')
