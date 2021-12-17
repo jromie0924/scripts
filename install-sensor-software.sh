307eacfc229efe918d79ba2a9c260a75e9fc93c5
Jackson Romie <jromie0924@gmail.com>
2021-12-16 23:36:45 -0700

    remove unused lines

--- install-sensor-software.sh
+++ install-sensor-software.sh
@@ -1,4 +1,4 @@
 pip install -U glances py3nvml
 pip install -U gpustat
 
-print('\n\nRUN GLANCES WITH THIS COMMAND: \nglances\n\nRUN GPUSTAT WITH THIS COMMAND:\ngpustat -i')
+printf "\n\nRUN GLANCES WITH THIS COMMAND: \nglances\n\nRUN GPUSTAT WITH THIS COMMAND:\ngpustat -i\n"
