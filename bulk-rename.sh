c81b2b46283eb771013cf0099d0ee5985feaf3a2
Jackson Romie <jromie0924@gmail.com>
2021-12-24 12:26:06 -0700

    script to bulk rename files in a directory

---
+++ bulk-rename.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage="Usage: bulk-rename.sh [EXTENSION] [DIRECTORY]"
+if [ -z "$1" ]
+then
+  printf "Required file extensionn\n\n"
+  printf "$usage"
+  exit 1
+fi
+if [ -z "$2" ]
+then
+  printf "Required directory.\n\n"
+  printf "$usage"
+  exit 1
+fi
+cd "$2"
+a=1
+for i in *.$1; do
+  new=$(printf "%05d.$1" "$a")
+  mv -i -- "$i" "$new"
+  let a=a+1
+done
+printf "Renamed $a files sequentially.\n"
+exit 0
