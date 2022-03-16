2f4d418e8c2f375479f6f6b635fef83813f7ed53
Jackson Romie <jromie0924@gmail.com>
2022-03-16 12:29:09 -0600

    updates

--- linux/toggle-touchpad.sh
+++ linux/toggle-touchpad.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/zsh
-
+re='^[0-9]+$'
 if [ -z $1 ]
 then
   printf "Required 1 for on or 0 for off as input argument.\n"
-else
-  device="$(xinput | grep -i touchpad | awk '{print $6}' | grep -o '[0-9]\+')"
-  xinput set-prop $device "Device Enabled" $1
+  exit 0
+fi
+device="$(xinput | grep -i touchpad | awk '{print $6}' | grep -o '[0-9]\+')"
+xinput set-prop $device "Device Enabled" $1
+if [ $1 -eq 1 ]
+then
+  printf "Touchpad enabled.\n"
+elif [ $1 -eq 0 ]
+then
+  printf "Touchpad disabled.\n"
 fi
 exit 0
