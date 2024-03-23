52224f7f4a58e1451da256c33cbe4203b2424c68
Jackson Romie <jromie0924@gmail.com>
2024-03-23 11:46:04 -0600

    add script to uninstall logitech options for *when* it fails

---
+++ mac/uninstall-options.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo /Library/Application\ Support/Logitech.localized/LogiOptionsPlus/logioptionsplus_agent.app/Contents/Frameworks/logioptionsplus_updater.app/Contents/MacOS/logioptionsplus_updater --full --uninstall
