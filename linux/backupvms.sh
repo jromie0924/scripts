3dddd42015dd202d75944da1f9e452fdc258d0ce
Jackson Romie <jromie0924@gmail.com>
2024-09-12 23:45:52 -0500

    fix bug

--- linux/backupvms.sh
+++ linux/backupvms.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+directory="${HOME}/green/VBoxBackup"
+if [ -z "$1" ]
+then
+  :
+else
+  directory="${1}"
+fi
+mkdir -p $directory
+
 # cut will split a string using the -d (delimiter) argument.
 # VBoxManage list vms will output:
 #
@@ -9,20 +19,8 @@
 # The second cut is cutting the string at the "}" and taking the FIRST field (-f1)
 # This allows us to expose only the VM identifier, and allows us to pass it as an
 # argument to the vboxmanage command to back up that VM.
-
-directory="${HOME}/green/VBoxBackup"
-if [ -z "$1" ]
-then
-  continue
-else
-  directory="${1}"
-fi
-
-mkdir -p $directory
-
 list=$(vboxmanage list vms | cut -d"{" -f2 | cut -d"}" -f1)
 while IFS= read -r line; do
-  # rm -f "${directory}/${line}.ova"
-  # vboxmanage export "${line}" -o "${directory}/${line}.ova"
-  echo "${directory}/${line}.ova"
+  rm -f "${directory}/${line}.ova"
+  vboxmanage export "${line}" -o "${directory}/${line}.ova"
 done <<< "$list"
